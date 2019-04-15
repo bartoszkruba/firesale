e2f4e4713be9754cdaf5f77a327ac565934c5fe0
bartoszkruba <bartosz.kruba@gmail.com>
2019-04-15 14:54:59 +0200

    Spring boot project initialization

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # firesale
 Auction webbapp created wtith Vue.js, Spring Boot and MySQL
+
+JDK: 11
+
+Indentetion:
+Java: 4/4/8
+JavaScript: 4/4/4
+HTML: 4/4/8 
---
+++ src/main/java/com/company/firesale/FiresaleApplication.java
@@ -0,0 +1,13 @@
+package com.company.firesale;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FiresaleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FiresaleApplication.class, args);
+    }
+
+}
