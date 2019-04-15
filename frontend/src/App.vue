35b673a4ff7f00852b3b0eb95719dedafea2d96b
bartoszkruba <bartosz.kruba@gmail.com>
2019-04-15 17:02:51 +0200

    Vue project - initial configuration done

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,38 +1,35 @@
 <template>
-  <v-app>
-    <v-toolbar app>
-      <v-toolbar-title class="headline text-uppercase">
-        <span>Vuetify</span>
-        <span class="font-weight-light">MATERIAL DESIGN</span>
-      </v-toolbar-title>
-      <v-spacer></v-spacer>
-      <v-btn
-        flat
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-      >
-        <span class="mr-2">Latest Release</span>
-      </v-btn>
-    </v-toolbar>
+    <v-app>
+        <v-toolbar app>
+            <v-toolbar-title class="headline text-uppercase">
+                <span>Vuetify</span>
+                <span class="font-weight-light">MATERIAL DESIGN</span>
+            </v-toolbar-title>
+            <v-spacer/>
+            <v-btn
+                    flat
+                    href="https://github.com/vuetifyjs/vuetify/releases/latest"
+                    target="_blank">
+                <span class="mr-2">Latest Release</span>
+            </v-btn>
+        </v-toolbar>
 
-    <v-content>
-      <router-view></router-view>
-    </v-content>
-  </v-app>
+        <v-content>
+            <router-view/>
+        </v-content>
+    </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
+    export default {
+        name: 'App',
+        components: {
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  },
-  data () {
-    return {
-      //
+        },
+        data() {
+            return {
+                //
+            }
+        }
     }
-  }
-}
 </script>
---
+++ src/main/java/com/company/firesale/config/MyWebMvcConfigurer.java
@@ -0,0 +1,21 @@
+package com.company.firesale.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class MyWebMvcConfigurer implements WebMvcConfigurer {
+
+   @Override
+   public void addCorsMappings(CorsRegistry registry) {
+      registry.addMapping("/**");
+   }
+
+   @Override
+   public void addResourceHandlers(ResourceHandlerRegistry registry) {
+      registry.addResourceHandler("*.*")
+              .addResourceLocations("file:src/main/resources/static/");
+   }
+}
