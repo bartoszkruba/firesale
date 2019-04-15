5ad8b1f2ba27985b97a8521913e415508f840598
bartoszkruba <bartosz.kruba@gmail.com>
2019-04-15 15:47:22 +0200

    Vue project - problem with vuetify

---
+++ frontend/src/main.js
@@ -0,0 +1,19 @@
+import Vue from 'vue'
+import './plugins/vuetify'
+import Vuetify from 'vuetify'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+
+import 'vuetify/dist/vuetify.min.css'
+
+Vue.use(Vuetify);
+
+Vue.config.productionTip = false;
+Vue.config.devtools = true;
+
+new Vue({
+  router,
+  store,
+  render: h => h(App)
+}).$mount('#app');
---
+++ frontend/src/vue.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    outputDir: './../src/main/resources/static',
+    devServer: {
+        proxy: {
+            '/api': {
+                target: 'http://localhost:8080/api',
+                ws: true,
+                changeOrigin: true,
+                pathRewrite: {
+                    '^/api': ''
+                }
+            }
+        }
+    }
+};
