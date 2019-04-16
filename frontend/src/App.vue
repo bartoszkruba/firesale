f8b3cbfef7f4dac5487c32b1467cb258a4fd9d73
Randy <anders.clark@gmail.com>
2019-04-16 10:41:28 +0200

    Menubar created.

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,29 +1,20 @@
 <template>
     <v-app>
-        <v-toolbar app>
-            <v-toolbar-title class="headline text-uppercase">
-                <span>Vuetify</span>
-                <span class="font-weight-light">MATERIAL DESIGN</span>
-            </v-toolbar-title>
-            <v-spacer/>
-            <v-btn
-                    flat
-                    href="https://github.com/vuetifyjs/vuetify/releases/latest"
-                    target="_blank">
-                <span class="mr-2">Latest Release</span>
-            </v-btn>
-        </v-toolbar>
+
 
         <v-content>
             <router-view/>
+            <Toolbar></Toolbar>
         </v-content>
     </v-app>
 </template>
 
 <script>
+    import Toolbar from "./components/Toolbar";
     export default {
         name: 'App',
         components: {
+            Toolbar
 
         },
         data() {
