a1de83444a52f9cf1f1b6ad9e762969f6763e81e
bartoszkruba <bartosz.kruba@gmail.com>
2019-04-17 15:29:26 +0200

    Login form connected to auth service and vuex

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -4,7 +4,13 @@ import Vuex from 'vuex'
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-    state: {},
-    mutations: {},
+    state: {
+        loggedIn: false
+    },
+    mutations: {
+        setLoggedIn(state, value) {
+            this.state.loggedIn = value;
+        }
+    },
     actions: {}
 })
--- frontend/src/views/Login.vue
+++ frontend/src/views/Login.vue
@@ -16,23 +16,27 @@
                                            contain
                                            height="100"></v-img>
                                     <v-form>
-                                        <v-text-field prepend-icon="person" name="login" label="Login"
-                                                      type="text" color="primary"></v-text-field>
+                                        <v-alert v-bind:value="showError" type="error">
+                                            Username and password doesn't match
+                                        </v-alert>
+                                        <v-text-field prepend-icon="person" name="usename" label="Username"
+                                                      type="text" color="primary" v-model="username"
+                                                      @keydown.enter="login"></v-text-field>
                                         <v-text-field prepend-icon="lock" name="password" label="Password"
-                                                      type="password" color="primary" id="password"></v-text-field>
+                                                      type="password" color="primary"
+                                                      v-model="password" @keydown.enter="login"></v-text-field>
                                     </v-form>
                                 </v-card-text>
                                 <v-card-actions>
-                                    <!--                                    <v-spacer></v-spacer>-->
                                     <v-layout align-center justify-center>
-                                        <v-btn color="primary" style="width: 100%">Login</v-btn>
+                                        <v-btn color="primary" style="width: 100% " @click="login">Login</v-btn>
                                     </v-layout>
                                 </v-card-actions>
                                 <v-toolbar class="mt-3">
                                     <v-layout align-center justify-center>
-                                        <v-toolbar-item>
-                                            New to FireSale? <a href="">Sign In</a>
-                                        </v-toolbar-item>
+                                        <v-toolbar-items name="signup">
+                                            <span>New to Firesale? <a href="">Register</a></span>
+                                        </v-toolbar-items>
                                     </v-layout>
                                 </v-toolbar>
                             </v-container>
@@ -45,15 +49,34 @@
 </template>
 
 <script>
+    import auth from '../services/authentication'
+
     export default {
-        name: "Login"
+
+
+        name: "Login",
+        data() {
+            return {
+                username: "",
+                password: "",
+                usernameError: "Username and password doesn't match",
+                showError: false
+            }
+        }
+        ,
+        methods: {
+            async login() {
+                let status = await auth.login(this.username, this.password);
+                this.showError = !status;
+                if (status) {
+                    this.$store.commit("setLoggedIn", true);
+                    this.$router.push("/")
+                }
+            }
+        }
     }
 </script>
 
 <style scoped>
-
-    .main-color {
-        background-color: #ffc400;
-    }
 
 </style>
