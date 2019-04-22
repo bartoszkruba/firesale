62cdb44ae614d018226d6e3bb0202f405c77ee2b
Randy <anders.clark@gmail.com>
2019-04-22 14:04:10 +0200

    Added proper vuetify structure to App.vue parent wrapper-elements.

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,9 +1,15 @@
 <template>
     <v-app>
         <v-content>
+            <v-container fluid fill-height>
+                <v-layout align-center justify-center>
+                    <v-flex xs12 sm8 md4>
             <router-view/>
             <Toolbar></Toolbar>
             <div id="bottomspacer"></div>
+                    </v-flex>
+                </v-layout>
+            </v-container>
         </v-content>
 
     </v-app>
