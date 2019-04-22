888b4ff1505fdf66353d5ce1d41d44442e5c3220
bartoszkruba <bartosz.kruba@gmail.com>
2019-04-22 17:58:31 +0200

    Form validation beginning

--- frontend/src/views/NewAuction.vue
+++ frontend/src/views/NewAuction.vue
@@ -8,14 +8,16 @@
                        accept="image/*"
                        multiple="multiple">
                 <h1>Create New Auction</h1>
-                <v-text-field label="Title" id="title" v-model="title"></v-text-field>
+                <v-text-field label="Title" id="title" v-model="title" :error-messages="titleError"></v-text-field>
+                <br>
                 <v-textarea label="Description" id="description" v-model="description"
-                            hint="Describe your product/products" outline></v-textarea>
+                            hint="Describe your product/products" outline
+                            :error-messages="descriptionError"></v-textarea>
                 <v-datetime-picker label="Closing At:" :datetime="closingTime"
-                                   @input="updateDateTime"></v-datetime-picker>
-                <v-text-field type="number" label="Startup Price (SEK)"
+                                   @input="updateDateTime" :error-messages="closingTimeError"></v-datetime-picker>
+                <v-text-field type="number" label="Startup Price (SEK)" :error-messages="startupPriceError"
                               v-model="startUpPrice"></v-text-field>
-                <v-text-field type="number" label="Buyout Price (SEK)"
+                <v-text-field type="number" label="Buyout Price (SEK)" :error-messages="buyoutPriceError"
                               v-model="buyOutPrice"></v-text-field>
                 <v-select :items="categories" label="Category" v-model="category"></v-select>
                 <h2>Images: ({{imagesCount}} selected)</h2>
@@ -44,6 +46,12 @@
                 startUpPrice: "",
                 buyOutPrice: "",
                 category: "All",
+
+                titleError: "Field cannot be empty",
+                descriptionError: "Field cannot be empty",
+                closingTimeError: "Choose date",
+                startupPriceError: "Field cannot be empty",
+                buyoutPriceError: "Field cannot be empty"
             }
         },
         computed: {
