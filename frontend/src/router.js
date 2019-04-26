862c41f5e728c326ab2830f7a188ebc6ef1c3bc7
Randy <anders.clark@gmail.com>
2019-04-26 11:24:44 +0200

    Added profile to router Added profile to profile-submenu Replace message with new auction on toolbar buttons

--- frontend/src/router.js
+++ frontend/src/router.js
@@ -6,6 +6,7 @@ import Register from './views/Register.vue'
 import NewAuction from './views/NewAuction.vue'
 import Auction from "./views/Auction";
 import About from "./views/About";
+import Profile from "./views/Profile";
 
 
 Vue.use(Router);
@@ -49,6 +50,10 @@ export default new Router({
             name: 'About',
             component: About
         },
-
+        {
+            path: '/profile',
+            name: 'Profile',
+            component: Profile
+        },
     ]
 })
