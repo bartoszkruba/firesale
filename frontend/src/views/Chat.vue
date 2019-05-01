5967616f0df782a91899440c933966b4cf09e57c
Martin <waldemarsson@gmail.com>
2019-05-01 15:54:16 +0200

    Chat view

--- frontend/src/views/Chat.vue
+++ frontend/src/views/Chat.vue
@@ -1,28 +1,42 @@
 <template>
     <v-container id="chat" style="height: 100vh;">
         <v-layout column fluid>
-            <v-container id="left-col" style="height: 100%;">
+            <v-container id="top-row" style="height: 100%; padding: 0;" white>
                 <v-container id="new-conversation">
-                    <v-text-field id="input-username" label="Username"></v-text-field>
-
-                </v-container>
-                <v-card id="conversation-list" v-for="conversation in this.getConversations" v-bind:key="conversation.id" color="primary">
-                        <h3 class="pa-4" @click="getMessagesInConversation(conversation.id)">  {{conversation.members[0].username}} </h3>
-                </v-card>
-            </v-container>
-            <v-container id="chat-window">
-                <v-container id="messages">
-                    <v-card v-for="message in messages" v-bind:key="message.id" v-show="messages.length > 0">
-                        <h3> {{message.textContent}}</h3>
-                        <p text-align="right"> {{ message.sender }} {{message.createdAt}}</p>
-                    </v-card>
-                </v-container>
-                <v-container id="bottom-row"    >
                     <v-layout row>
-                    <v-text-field v-model="newMessage"></v-text-field>
-                    <v-btn><v-icon>plus</v-icon></v-btn>
+                        <v-text-field id="input-username" label="Username" pa-1></v-text-field>
+                        <v-icon medium> chat</v-icon>
                     </v-layout>
                 </v-container>
+                <v-layout column align-center>
+                    <v-container v-show="showConversationList">
+                        <v-card id="conversation-list" v-for="conversation in this.getConversations"
+                                v-bind:key="conversation.id" color="primary" style="width: 100%;">
+                            <h3 class="pa-4" @click="getMessagesInConversation(conversation.id)">
+                                {{conversation.members[0].username}} </h3>
+                        </v-card>
+                    </v-container>
+                    <v-icon large @click="toggleConversationList">keyboard_arrow_down</v-icon>
+                </v-layout>
+            </v-container>
+            <v-container id="chat-window" class="pa-0 mt-1 mb-1" v-show="messages.length > 0">
+<!--                <v-container id="messages" class="pa-0 pt-2">-->
+                    <v-card v-for="message in messages" v-bind:key="message.id"
+                            class="pa-1">
+
+                        <h3> {{message.textContent}}</h3>
+                        <p class="ma-0"> {{ message.username }} {{message.createdAt}}</p>
+
+                    </v-card>
+<!--                </v-container>-->
+                    <v-container id="bottom-row" white class="mt-1">
+                        <v-layout row>
+                            <v-text-field v-model="newMessage" label="Message"></v-text-field>
+                            <v-icon>send</v-icon>
+                        </v-layout>
+                    </v-container>
+
+
             </v-container>
         </v-layout>
     </v-container>
@@ -33,33 +47,35 @@
 
     export default {
         name: "Chat",
-        data(){
+        data() {
             return {
                 newMessage: '',
                 messages: [],
+                showConversationList: false,
             }
         },
         methods: {
-            async getMessagesInConversation(id){
+            async getMessagesInConversation(id) {
                 await conversationService.getMessagesInConversation(id).then(response => {
                     this.messages = response.data;
                 });
-                console.log(this.messages);
+                this.toggleConversationList();
+            },
+            toggleConversationList() {
+                this.showConversationList = !this.showConversationList;
             }
         },
         computed: {
-            getConversations(){
+            getConversations() {
                 return this.$store.state.conversations;
             },
         },
-        beforeMount(){
+        beforeMount() {
             this.$store.dispatch('getConversations');
         }
     }
 </script>
 
 <style scoped>
-    #left-col{
-        background-color: white;
-    }
+
 </style>
